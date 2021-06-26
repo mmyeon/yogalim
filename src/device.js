4805fe68952422ee2ec0a43a5b2b075ee56cba02
mmyeon <mindfulyeon@gmail.com>
2021-06-26 23:16:59 +0900

    [style] 기기 크기에 따라 배경이미지 달라지도록 수정

---
+++ src/device.js
@@ -0,0 +1,7 @@
+export const DEVICE_WIDTH_TABLET = 768;
+export const DEVICE_WIDTH_DESKTOP = 1024;
+
+export const device = {
+  tablet: `(min-width: ${DEVICE_WIDTH_TABLET}px)`,
+  desktop: `(min-width: ${DEVICE_WIDTH_DESKTOP}px)`,
+};
