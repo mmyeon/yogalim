857558d84ee6190eb222fce9bd8db2c0f2a90886
mmyeon <mindfulyeon@gmail.com>
2021-06-28 18:24:33 +0900

    [feat] PracticeVideoList 컴포넌트 > params에 따라 한글 타이틀을 다이나믹하게 보여주기

--- src/routes/PracticeVideoList/index.js
+++ src/routes/PracticeVideoList/index.js
@@ -1,12 +1,34 @@
 import React from "react";
+import { useParams } from "react-router";
 import Header from "../../components/Header";
 import * as Styled from "./index.styles";
+import data from "../../data";
 
 const VideoList = () => {
+  const params = useParams();
+  const bodyPartChoice = data.find(
+    (bodyPart) => bodyPart.title.eng === params.body
+  ).title.kor;
+
   return (
     <Styled.Container>
       <div className="background">
         <Header />
+        <div className="content">
+          <h2 className="title">
+            {bodyPartChoice === "목" ||
+            bodyPartChoice === "전신" ||
+            bodyPartChoice === "골반" ? (
+              <>
+                <b>{bodyPartChoice}</b>이 편안한 요가
+              </>
+            ) : (
+              <>
+                <b>{bodyPartChoice}</b>가 편안한 요가
+              </>
+            )}
+          </h2>
+        </div>
       </div>
     </Styled.Container>
   );
