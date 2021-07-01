43add34dfd01f3cd336a67b410842d6197f7d181
mmyeon <mindfulyeon@gmail.com>
2021-07-01 13:01:27 +0900

    [feat] PracticeVideoList 컴포넌트 > data에서 thumbnail 경로 가져와서 화면에 뿌리기

--- src/routes/PracticeVideoList/index.js
+++ src/routes/PracticeVideoList/index.js
@@ -1,16 +1,21 @@
-import React, { useRef } from "react";
-import { useParams } from "react-router";
+import React from "react";
+import { useParams } from "react-router-dom";
 import Header from "../../components/Header";
 import * as Styled from "./index.styles";
 import data from "../../data";
 
 const VideoList = () => {
   const params = useParams();
-  const bodyPartChoice = data.find(
+  const selectedBodyPartObj = data.find(
     (bodyPart) => bodyPart.title.eng === params.body
-  ).title.kor;
+  );
 
-  const videoListEl = useRef(null);
+  const titleInKorean = selectedBodyPartObj.title.kor;
+  // const bodyPartChoice = data.find(
+  //   (bodyPart) => bodyPart.title.eng === params.body
+  // ).title.kor;
+
+  // console.log("bodyPartChoice", bodyPartChoice);
 
   return (
     <Styled.Container>
@@ -18,28 +23,28 @@ const VideoList = () => {
         <Header />
         <div className="content">
           <h2 className="title">
-            {bodyPartChoice === "목" ||
-            bodyPartChoice === "전신" ||
-            bodyPartChoice === "골반" ? (
+            {titleInKorean === "목" ||
+            titleInKorean === "전신" ||
+            titleInKorean === "골반" ? (
               <>
-                <b>{bodyPartChoice}</b>이 편안해지는 요가
+                <b>{titleInKorean}</b>이 편안해지는 요가
               </>
             ) : (
               <>
-                <b>{bodyPartChoice}</b>가 편안해지는 요가
+                <b>{titleInKorean}</b>가 편안해지는 요가
               </>
             )}
           </h2>
           <div className="video-list">
-            <div
-              className="video-container"
-              ref={videoListEl}
-              onScroll={() => console.log("미연쓰")}
-            >
-              <div className="video"></div>
-              <div className="video"></div>
-              <div className="video"></div>
-              <div className="video"></div>
+            <div className="video-container">
+              {selectedBodyPartObj.playList.map((item) => (
+                <img
+                  key={item.videoId}
+                  className="video"
+                  src={item.thumbnail}
+                  alt="yoga video thumbnail"
+                />
+              ))}
             </div>
           </div>
         </div>
