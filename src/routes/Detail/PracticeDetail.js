f0a3ba79db89cf7c3ef6fa19c822eb9b94299387
mmyeon <mindfulyeon@gmail.com>
2021-07-01 20:07:22 +0900

    [style] 물결 배경이미지 추가

--- src/routes/Detail/PracticeDetail.js
+++ src/routes/Detail/PracticeDetail.js
@@ -12,6 +12,15 @@ const StyledPracticeDetail = styled.div`
   > .background-video {
     position: relative;
     margin-left: -10em;
+    z-index: -2;
+  }
+
+  > svg {
+    position: absolute;
+    bottom: -8em;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
   }
 `;
 
@@ -27,6 +36,18 @@ const PracticeDetail = () => {
         url={`https://youtu.be/${videoId}`}
         muted
       />
+      <svg
+        width="320"
+        height="321"
+        viewBox="0 0 320 321"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M359.993 505.452L362.258 123.406C362.258 123.406 330.431 54.4813 247.509 61.9659C164.586 69.4504 147.143 112.307 47.5491 32.5417C47.5491 32.5417 12.4456 -0.802887 -52.0173 0.0326856L-55 502.991L152.496 504.222L359.993 505.452Z"
+          fill="#39AE76"
+        />
+      </svg>
     </StyledPracticeDetail>
   );
 };
