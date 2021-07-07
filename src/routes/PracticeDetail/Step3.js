1a58825c1b146bbdb15c0e4952240952fb6fc15b
mmyeon <mindfulyeon@gmail.com>
2021-07-07 20:47:30 +0900

    [feat] 동영상 재생 종료 시간 설정하기

--- src/routes/PracticeDetail/Step3.js
+++ src/routes/PracticeDetail/Step3.js
@@ -1,22 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactPlayer from "react-player";
 import styled from "styled-components";
 
 const StyledStep3 = styled.div``;
 
 const Step3 = ({ goNextStep, currentVideoId }) => {
+  const [duration, setDuration] = useState(0);
+  const [played, setPlayed] = useState(0);
+
+  useEffect(() => {
+    if (played && duration) {
+      const remain = Math.round(duration * (1 - played));
+
+      if (remain <= 20) {
+        goNextStep();
+      }
+    }
+  }, [played, duration]);
+
   return (
     <StyledStep3>
       <ReactPlayer
         playing
         width="100vw"
         height="100vh"
-        url={`https://youtu.be/${currentVideoId}`}
-        onEnded={goNextStep}
         controls
+        url={`https://youtu.be/${currentVideoId}`}
+        // 동영상 played시간
+        onProgress={handleProgress}
+        // 동영상 재생 시간
+        onDuration={handleDuration}
       />
     </StyledStep3>
   );
+
+  function handleDuration(duration) {
+    setDuration(duration);
+  }
+
+  function handleProgress(state) {
+    setPlayed(state.played);
+  }
 };
 
 export default Step3;
