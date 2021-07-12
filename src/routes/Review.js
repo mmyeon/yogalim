be668b82054bf4bde8215ecd0d9825e88debb812
mmyeon <mindfulyeon@gmail.com>
2021-07-12 17:08:11 +0900

    [feat] Review 컴포넌트 추가

--- src/routes/Review.js
+++ src/routes/Review.js
@@ -1,7 +1,29 @@
 import React from "react";
+import Card from "../components/Card";
+import Template from "../components/Template";
 
 const Review = () => {
-  return <div>수련 변화 리뷰</div>;
+  return (
+    <Template
+      title={
+        <>
+          몸과 마음의 변화가
+          <br />
+          <span className="thinner">느껴지시나요?</span>
+        </>
+      }
+      body={
+        <Card
+          title={
+            <>
+              수련 전 / 후 <br />
+              변화를 살펴볼까요?
+            </>
+          }
+        />
+      }
+    />
+  );
 };
 
 export default Review;
