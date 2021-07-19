f1fb20e857286967768f31d508699b6c77180c14
mmyeon <mindfulyeon@gmail.com>
2021-07-19 15:31:30 +0900

    [add] 비디오 썸네일 이미지 추가

--- src/data.js
+++ src/data.js
@@ -11,7 +11,8 @@ const data = [
         // TODO: 시간 어떻게 가져올 것인가, youtube api로 가져오기
         time: "20분",
         thumbnail: "/assets/images/practiceVideoList/test.png",
-        videoId: "NWKtNDqBDmo?t=2800",
+        videoId: "NWKtNDqBDmo",
+        videoClip: "NWKtNDqBDmo?t=2800",
         url: "https://www.youtube.com/watch?v=bqmlyel0kPg&t=1s",
       },
       {
@@ -63,16 +64,19 @@ const data = [
         title: "Resilience: whole body yoga practice",
         desc: "This 60-minute practice takes time to “oil up” the entire body, to slow down the breath and to look within. It is a balanced whole-body practice that helps you cultivate and maintain physical, energetic and emotional resilience.",
         // time: "",
-        thumbnail: "/assets/images/practiceVideoList/test.png",
+        thumbnail:
+          "/assets/images/practiceVideoList/thumbnails/wholeBody/whole-body1.png",
+        url: "",
         // 11초 전에 종료
       },
       {
         videoId: "HNn63Jbm1PY?t=43",
-        videoClip: "HNn63Jbm1PY?t=1180",
+        videoClip: "HNn63Jbm1PY?t=1193",
         title: "Empower yoga practice",
         desc: "This yoga practice uses expansive, open body positions, 'tidal wave' breathing pattern with emphasis on inhalation, and 'removing the obstacles' meditation to help you cultivate a sense of powerful presence. ",
         // time: "",
-        thumbnail: "/assets/images/practiceVideoList/test.png",
+        thumbnail:
+          "/assets/images/practiceVideoList/thumbnails/wholeBody/whole-body2.png",
         // 11초 전에 종료
       },
       {
@@ -81,16 +85,18 @@ const data = [
         title: "Yoga practice for core awareness and strength",
         desc: "This short yoga practice will help you become aware of the center of your body and teach you how to move from that center. It will also help you strengthen your core and create stability and integration for whatever daily tasks you want to do.",
         // time: "",
-        thumbnail: "/assets/images/practiceVideoList/test.png",
+        thumbnail:
+          "/assets/images/practiceVideoList/thumbnails/wholeBody/whole-body3.png",
         // 13초 전에 종료
       },
       {
         videoId: "_waBrv-FpcU?t=60",
-        videoClip: "_waBrv-FpcU?t=1755",
+        videoClip: "_waBrv-FpcU?t=1790",
         title: "Explore the asymmetries practice",
         desc: "Before you can do anything about your body asymmetries you need to figure out what they are. This yoga practice will help you compare the right and the left side of the body and use lateral bending to begin restore balance between the two sides.",
         // time: "",
-        thumbnail: "/assets/images/practiceVideoList/test.png",
+        thumbnail:
+          "/assets/images/practiceVideoList/thumbnails/wholeBody/whole-body4.png",
         // 14초 전에 종료
       },
     ],
