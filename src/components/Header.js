2d3844c25a49b10f2c69d4c0cc87575651a74f3d
mmyeon <mindfulyeon@gmail.com>
2021-07-23 16:43:35 +0900

    [feat] Header 컴포넌트에 BackButton 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,32 @@
 import React from "react";
 import styled from "styled-components";
+import BackButton from "./buttons/BackButton";
 import Logo from "./Logo";
 
 const StyledHeader = styled.header`
   position: absolute;
   top: 1.5em;
   width: 100%;
+  height: 2em;
   text-align: center;
+
+  > .backButton {
+    position: absolute;
+    left: 1em;
+    top: 0;
+  }
+
+  > .logo {
+    position: relative;
+    top: 50%;
+    transform: translateY(-50%);
+  }
 `;
 
 const Header = () => {
   return (
     <StyledHeader>
+      <BackButton />
       <Logo />
     </StyledHeader>
   );
