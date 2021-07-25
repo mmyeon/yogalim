2c44b5ae79abdaf47c732c4d9b6ccec0055a00d4
mmyeon <mindfulyeon@gmail.com>
2021-07-25 22:44:22 +0900

    [refactor] data 가독성 개선

--- src/data.js
+++ src/data.js
@@ -7,48 +7,56 @@ const data = [
     buttonColor: `${COLORS.darkGreen1}`,
     playList: [
       {
-        id: "neck1",
-        videoId: "LIEMiQiXBPE?t=49",
-        videoClip: "LIEMiQiXBPE?t=598",
+        videoId: "LIEMiQiXBPE",
+        videoTime: {
+          start: "49",
+          highlight: "598",
+          end: "12",
+        },
         title: "Follow the sun: Happy neck yoga practice",
         desc: "This short yoga practice deals with neck stiffness. You can do this practice even when you are tired, since all poses included here are done pretty close to the ground. The practice ends with Sunflower meditation that invites you to turn your face toward the light and absorb it into your body.",
         duration: "21분",
         thumbnail: "/assets/images/practiceVideoList/thumbnails/neck/neck1.png",
         url: "https://youtu.be/LIEMiQiXBPE?t=49",
-        // 12초 전에 종료
       },
       {
-        id: "neck2",
-        videoId: "SqjhjyWQ1TA?t=39",
-        videoClip: "SqjhjyWQ1TA?t=860",
+        videoId: "SqjhjyWQ1TA",
+        videoTime: {
+          start: "39",
+          highlight: "860",
+          end: "5",
+        },
         title: "Standing yoga practice for neck and upper back tension",
         desc: "This yoga practice is done entirely in the standing position. It targets the neck and upper back, and includes various movements that both increase circulation to those areas and stretch them. It also includes a simple breathing practice that helps you relax your neck and lengthen your spine. This practice is a perfect match for not-so-perfect practice spaces.",
         duration: "32분",
         thumbnail: "/assets/images/practiceVideoList/thumbnails/neck/neck2.png",
         url: "https://youtu.be/SqjhjyWQ1TA?t=39",
-        // 5초 전에 종료
       },
       {
-        id: "neck3",
-        videoId: "TH-Nbk-HUos?t=32",
-        videoClip: "TH-Nbk-HUos?t=975",
+        videoId: "TH-Nbk-HUos",
+        videoTime: {
+          start: "32",
+          highlight: "975",
+          end: "9",
+        },
         title: "Neck and upper back assessment yoga practice",
         desc: "This short yoga practice will help you assess the sensations in your neck and upper back to get a better idea about where the tension is and what is the best way to deal with it. ",
         duration: "26분",
         thumbnail: "/assets/images/practiceVideoList/thumbnails/neck/neck3.png",
         url: "https://youtu.be/TH-Nbk-HUos?t=32",
-        // 9초 전에 종료
       },
       {
-        id: "neck4",
-        videoId: "wMutSyH85o4?t=15",
-        videoClip: "wMutSyH85o4?t=595",
+        videoId: "wMutSyH85o4",
+        videoTime: {
+          start: "15",
+          highlight: "595",
+          end: "1",
+        },
         title: "Relieve neck tension",
         desc: "",
         duration: "29분",
         thumbnail: "/assets/images/practiceVideoList/thumbnails/neck/neck4.png",
         url: "https://youtu.be/wMutSyH85o4?t=15",
-        // 1초 전에 종료
       },
     ],
   },
@@ -58,52 +66,60 @@ const data = [
     buttonColor: `${COLORS.primaryTeal}`,
     playList: [
       {
-        id: "shoulder1",
-        videoId: "LlLQpxo5pmQ?t=54",
-        videoClip: "LlLQpxo5pmQ?t=669",
+        videoId: "LlLQpxo5pmQ",
+        videoTime: {
+          start: "54",
+          highlight: "669",
+          end: "19",
+        },
         title: "Realign your shoulder girdle yoga practice",
         desc: "This yoga practice will make you aware of your shoulder girdle, relieve upper back tension and stretch the muscles that bind the shoulder blades to the ribcage and spine.",
         duration: "31분",
         thumbnail:
           "/assets/images/practiceVideoList/thumbnails/shoulder/shoulder1.png",
         url: "https://youtu.be/LlLQpxo5pmQ?t=54",
-        // 19초 전에 종료
       },
       {
-        id: "shoulder2",
-        videoId: "Nvq0uEC3VDM?t=54",
-        videoClip: "Nvq0uEC3VDM?t=746",
+        videoId: "Nvq0uEC3VDM",
+        videoTime: {
+          start: "54",
+          highlight: "746",
+          end: "15",
+        },
         title: "Liberate your shoulders",
         desc: "The shoulder often ends up on the receiving end of what’s happening elsewhere in the body. This yoga practice addresses shoulder discomfort by working with the chest and upper back, neck and middle back instead of the shoulder joint itself. It will help you liberate your shoulders from the incessant pulling of the surrounding structures.",
         duration: "41분",
         thumbnail:
           "/assets/images/practiceVideoList/thumbnails/shoulder/shoulder2.png",
         url: "https://youtu.be/Nvq0uEC3VDM?t=54",
-        // 15초 전에 종료
       },
       {
-        id: "shoulder3",
-        videoId: "E-E76UQ3fos?t=158",
-        videoClip: "E-E76UQ3fos?t=2662",
+        videoId: "E-E76UQ3fos",
+        videoTime: {
+          start: "158",
+          highlight: "2662",
+          end: "8",
+        },
         title: "Body appreciation yoga practice",
         desc: "Our bodies try to communicate with us, but we rarely listen. We are so focused on the external environment, that we tend to ignore our inner state until it complains loud enough through pain, physiological distress or mental anguish. Instead of letting things fester and get out of hand, let’s learn to listen to our body’s signals. In this full-length yoga practice, we will focus on developing our interoception (conscious awareness of what’s going on inside) and balancing our physiology, while expressing appreciation to our bodies for all the work they do for us every day.",
         duration: "73분",
         thumbnail:
           "/assets/images/practiceVideoList/thumbnails/shoulder/shoulder3.png",
         url: "https://youtu.be/E-E76UQ3fos?t=158",
-        // 8초 전에 종료
       },
       {
-        id: "shoulder4",
-        videoId: "OpYljvPPGcw?t=59",
-        videoClip: "OpYljvPPGcw?t=405",
+        videoId: "OpYljvPPGcw",
+        videoTime: {
+          start: "59",
+          highlight: "405",
+          end: "8",
+        },
         title: "Short yoga practice for energetic boost",
         desc: "This 20-minute yoga practice is designed to give you an energetic boost if you feel tired, depleted or frazzled. You can do it in the morning or at any point during the day; however, please do not do it too close to bedtime. The entire practice is focused on expanding and deepening your inhalation, which creates the sense of spaciousness and helps build your energy up.",
         duration: "21분",
         thumbnail:
           "/assets/images/practiceVideoList/thumbnails/shoulder/shoulder4.png",
         url: "https://youtu.be/OpYljvPPGcw?t=59",
-        // 8초 전에 종료
       },
     ],
   },
@@ -113,52 +129,60 @@ const data = [
     buttonColor: `${COLORS.brightTurquoise}`,
     playList: [
       {
-        id: "spine1",
-        videoId: "h7ejzxyosMo?t=138",
-        videoClip: "h7ejzxyosMo?t=1262",
+        videoId: "h7ejzxyosMo",
+        videoTime: {
+          start: "138",
+          highlight: "1262",
+          end: "10",
+        },
         title: "Exercise your diaphragm",
         desc: "This yoga practice helps you increase awareness of the movement of your diaphragm, explore the role of the diaphragm in spinal stabilization and relieve ribcage stiffness.",
         duration: "54분",
         thumbnail:
           "/assets/images/practiceVideoList/thumbnails/spine/spine1.png",
         url: "https://youtu.be/h7ejzxyosMo?t=138",
-        // 10초 전에 종료
       },
       {
-        id: "spine2",
-        videoId: "xd9gl_rkzE0?t=68",
-        videoClip: "xd9gl_rkzE0?t=1435",
+        videoId: "xd9gl_rkzE0",
+        videoTime: {
+          start: "68",
+          highlight: "1435",
+          end: "14",
+        },
         title: "Improve your posture yoga practice",
         desc: "Counteract the stooped posture by strengthening the muscles that support the body in the upright position, relieve neck tension and realign the spinal curves to improve the posture.",
         duration: "41분",
         thumbnail:
           "/assets/images/practiceVideoList/thumbnails/spine/spine2.png",
         url: "https://youtu.be/xd9gl_rkzE0?t=68",
-        // 14초 전에 종료
       },
       {
-        id: "spine3",
-        videoId: "dGUyhuUrA4o?t=48",
-        videoClip: "dGUyhuUrA4o?t=286",
+        videoId: "dGUyhuUrA4o",
+        videoTime: {
+          start: "48",
+          highlight: "286",
+          end: "11",
+        },
         title: "Chair yoga practice for supple spine",
         desc: "This yoga practice takes the spine through the full range of motion and is useful for neck and upper back tension. The entire practice is done sitting in a chair, and you can easily add to it by including standing yoga poses.",
         duration: "34분",
         thumbnail:
           "/assets/images/practiceVideoList/thumbnails/spine/spine3.png",
         url: "https://youtu.be/dGUyhuUrA4o?t=48",
-        // 11초 전에 종료
       },
       {
-        id: "spine4",
-        videoId: "OOqNIh5R3Z0?t=78",
-        videoClip: "OOqNIh5R3Z0?t=958",
+        videoId: "OOqNIh5R3Z0",
+        videoTime: {
+          start: "78",
+          highlight: "958",
+          end: "17",
+        },
         title: "Train your static and dynamic balance yoga practice",
         desc: "This yoga practice contains a number of simple standing poses that will help you train both static and dynamic balance. Feel free to modify the poses in a way that is appropriate for you (for example, it is completely up to you how deep you go into Warrior 3). Please pay attention to the breath and see if you can use your breath to create more length in your spine and more support via progressive abdominal contraction on exhale.",
         duration: "48분",
         thumbnail:
           "/assets/images/practiceVideoList/thumbnails/spine/spine4.png",
         url: "https://youtu.be/OOqNIh5R3Z0?t=78",
-        // 17초 전에 종료
       },
     ],
   },
@@ -168,52 +192,60 @@ const data = [
     buttonColor: `${COLORS.amber}`,
     playList: [
       {
-        id: "pelvis1",
-        videoId: "E2ynCj6_6DM?t=278",
-        videoClip: "E2ynCj6_6DM?t=1402",
+        videoId: "E2ynCj6_6DM",
+        videoTime: {
+          start: "278",
+          highlight: "1402",
+          end: "6",
+        },
         title: "Retain nourishment and tone your diaphragm and pelvic floor",
         desc: "In this yoga practice we focus on increasing tonicity both in your diaphragm and your pelvic floor to provide visceral massage to your digestive organs and to help them retain nourishment. This practice will also help you tone your diaphragm and pelvic floor muscles, strengthen your core, stabilize your lumbar spine and reestablish the link between Prana and Apana Vayus to enhance vitality in your system.",
         duration: "57분",
         thumbnail:
           "/assets/images/practiceVideoList/thumbnails/pelvis/pelvis1.png",
         url: "https://youtu.be/E2ynCj6_6DM?t=278",
-        // 6초 전에 종료
       },
       {
-        id: "pelvis2",
-        videoId: "PRTSdgueVzU?t=45",
-        videoClip: "PRTSdgueVzU?t=208",
+        videoId: "PRTSdgueVzU",
+        videoTime: {
+          start: "45",
+          highlight: "208",
+          end: "7",
+        },
         title: "Short yoga practice for hip flexors",
         desc: "This short yoga practice for hip flexors is meant to loosen up your hips by contracting and stretching your hip flexors. This can have a positive effect on your lower back as well, since hip flexor tightness is often connected to lower back pain.",
         duration: "15분",
         thumbnail:
           "/assets/images/practiceVideoList/thumbnails/pelvis/pelvis2.png",
         url: "https://youtu.be/PRTSdgueVzU?t=45",
-        // 7초 전에 종료
       },
       {
-        id: "pelvis3",
-        videoId: "nAat1hyTT8A?t=60",
-        videoClip: "nAat1hyTT8A?t=1742",
+        videoId: "nAat1hyTT8A",
+        videoTime: {
+          start: "60",
+          highlight: "1742",
+          end: "18",
+        },
         title: "Yoga practice for abductors and adductors",
         desc: "We take our ability to walk for granted, until it becomes problematic. Every step we take is a delicate dance between multiple muscle groups.",
         duration: "33분",
         thumbnail:
           "/assets/images/practiceVideoList/thumbnails/pelvis/pelvis3.png",
         url: "https://youtu.be/nAat1hyTT8A?t=60",
-        // 18초 전에 종료
       },
       {
-        id: "pelvis4",
-        videoId: "39TuBoRmqWo?t=62",
-        videoClip: "39TuBoRmqWo?t=1828",
+        videoId: "39TuBoRmqWo",
+        videoTime: {
+          start: "62",
+          highlight: "1828",
+          end: "7",
+        },
         title: "Strong yoga practice for hips and hamstrings",
         desc: "The yoga practice includes a wide range of hip movements and invites you to go deeper in stages. The practice gradually builds toward Upavista Konasana and ensures that both your hips and your hamstrings are ready for it. It is a pretty strong practice that includes fairly long holds in some challenging poses.",
         duration: "63분",
         thumbnail:
           "/assets/images/practiceVideoList/thumbnails/pelvis/pelvis4.png",
         url: "https://youtu.be/39TuBoRmqWo?t=62",
-        // 7초 전에 종료
       },
     ],
   },
@@ -223,48 +255,56 @@ const data = [
     buttonColor: `${COLORS.brown}`,
     playList: [
       {
-        id: "legs1",
-        videoId: "0W32GqNyny4?t=60",
-        videoClip: "0W32GqNyny4?t=1236",
+        videoId: "0W32GqNyny4",
+        videoTime: {
+          start: "60",
+          highlight: "1236",
+          end: "1",
+        },
         title: "Yoga for hamstring pain and tightness",
         desc: "TDon't stretch your hamstrings if you have a hamstring injury! Use this 3-stage yoga practice to heal the hamstring injury gradually and release chronic tension.",
         duration: "26분",
         thumbnail: "/assets/images/practiceVideoList/thumbnails/legs/legs1.png",
         url: "https://youtu.be/0W32GqNyny4?t=60",
-        // 1초 전에 종료
       },
       {
-        id: "legs2",
-        videoId: "kaoZMagv9hs?t=62",
-        videoClip: "kaoZMagv9hs?t=1611",
+        videoId: "kaoZMagv9hs",
+        videoTime: {
+          start: "62",
+          highlight: "1611",
+          end: "16",
+        },
         title: "Yoga practice for knee stability",
         desc: "This yoga practices facilitates a balanced relationship between the quads, hamstrings, adductors and abductors to support and stabilize the knees.",
         duration: "47분",
         thumbnail: "/assets/images/practiceVideoList/thumbnails/legs/legs2.png",
         url: "https://youtu.be/kaoZMagv9hs?t=62",
-        // 16초 전에 종료
       },
       {
-        id: "legs3",
-        videoId: "eIsWFiFfR1Q?t=293",
-        videoClip: "eIsWFiFfR1Q?t=729",
+        videoId: "eIsWFiFfR1Q",
+        videoTime: {
+          start: "293",
+          highlight: "729",
+          end: "7",
+        },
         title: "Move your lymph and strengthen your ankles",
         desc: "In this practice we use different adaptations of Chair pose with ankle movement to facilitate a steady lymph flow throughout the system to support our immune function. The practice includes other yoga poses that serve the same purpose, as well as breath awareness and guided relaxation at the end. This practice also helps to warm up and strengthen your ankles, knees, thighs and hips, while awakening your upper back and neck, so it is very useful if you sit a lot during the day.",
         duration: "39분",
         thumbnail: "/assets/images/practiceVideoList/thumbnails/legs/legs3.png",
         url: "https://youtu.be/eIsWFiFfR1Q?t=293",
-        // 7초 전에 종료
       },
       {
-        id: "legs4",
-        videoId: "pEb2lokZ2UI?t=48",
-        videoClip: "pEb2lokZ2UI?t=228",
+        videoId: "pEb2lokZ2UI",
+        videoTime: {
+          start: "48",
+          highlight: "228",
+          end: "5",
+        },
         title: "Feel the earth: Happy ankles yoga practice",
         desc: "In this practice you will focus your attention on stretching and strengthening your ankles, but at the end of the practice, please take note whether or not it affects your entire structural organization. Notice how your neck, upper and lower back feel as a result.",
         duration: "24분",
         thumbnail: "/assets/images/practiceVideoList/thumbnails/legs/legs4.png",
         url: "https://youtu.be/pEb2lokZ2UI?t=48",
-        // 5초 전에 종료
       },
     ],
   },
@@ -274,8 +314,12 @@ const data = [
     buttonColor: `${COLORS.red}`,
     playList: [
       {
-        id: "whole-body1",
-        videoId: "rzdtYf1Zwes?t=34",
+        videoId: "rzdtYf1Zwes",
+        videoTime: {
+          start: "34",
+          highlight: "1807",
+          end: "11",
+        },
         videoClip: "rzdtYf1Zwes?t=1807",
         title: "Resilience: whole body yoga practice",
         desc: "This 60-minute practice takes time to “oil up” the entire body, to slow down the breath and to look within. It is a balanced whole-body practice that helps you cultivate and maintain physical, energetic and emotional resilience.",
@@ -283,43 +327,50 @@ const data = [
         thumbnail:
           "/assets/images/practiceVideoList/thumbnails/wholeBody/whole-body1.png",
         url: "https://youtu.be/rzdtYf1Zwes?t=34",
-        // 11초 전에 종료
       },
       {
-        id: "whole-body2",
-        videoId: "HNn63Jbm1PY?t=43",
-        videoClip: "HNn63Jbm1PY?t=1193",
+        videoId: "HNn63Jbm1PY",
+        videoTime: {
+          start: "43",
+          highlight: "1193",
+          end: "11",
+        },
         title: "Empower yoga practice",
         desc: "This yoga practice uses expansive, open body positions, 'tidal wave' breathing pattern with emphasis on inhalation, and 'removing the obstacles' meditation to help you cultivate a sense of powerful presence. ",
         duration: "51분",
         thumbnail:
           "/assets/images/practiceVideoList/thumbnails/wholeBody/whole-body2.png",
         url: "https://youtu.be/HNn63Jbm1PY?t=43",
-        // 11초 전에 종료
       },
       {
         id: "whole-body3",
-        videoId: "m7NRc0A-2hU?t=57",
-        videoClip: "m7NRc0A-2hU?t=360",
+        videoId: "m7NRc0A-2hU",
+        videoTime: {
+          start: "57",
+          highlight: "360",
+          end: "13",
+        },
         title: "Yoga practice for core awareness and strength",
         desc: "This short yoga practice will help you become aware of the center of your body and teach you how to move from that center. It will also help you strengthen your core and create stability and integration for whatever daily tasks you want to do.",
         duration: "24분",
         thumbnail:
           "/assets/images/practiceVideoList/thumbnails/wholeBody/whole-body3.png",
         url: "https://youtu.be/m7NRc0A-2hU?t=57",
-        // 13초 전에 종료
       },
       {
         id: "whole-body4",
-        videoId: "_waBrv-FpcU?t=60",
-        videoClip: "_waBrv-FpcU?t=1790",
+        videoId: "_waBrv-FpcU",
+        videoTime: {
+          start: "60",
+          highlight: "1790",
+          end: "14",
+        },
         title: "Explore the asymmetries practice",
         desc: "Before you can do anything about your body asymmetries you need to figure out what they are. This yoga practice will help you compare the right and the left side of the body and use lateral bending to begin restore balance between the two sides.",
         duration: "42분",
         thumbnail:
           "/assets/images/practiceVideoList/thumbnails/wholeBody/whole-body4.png",
         url: "https://youtu.be/_waBrv-FpcU?t=60",
-        // 14초 전에 종료
       },
     ],
   },
