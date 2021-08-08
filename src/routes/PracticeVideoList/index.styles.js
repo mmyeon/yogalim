15936756faf27bd19ddb6ab76797f6ffa259723d
mmyeon <mindfulyeon@gmail.com>
2021-08-08 20:36:02 +0900

    [fix] PracticeVideoList 컴포넌트 > 배경 그라디언트 박스사이즈에 맞도록 수정

--- src/routes/PracticeVideoList/index.styles.js
+++ src/routes/PracticeVideoList/index.styles.js
@@ -77,7 +77,7 @@ export const Container = styled.div`
                 top: 0;
                 left: 0;
                 width: 100%;
-                height: 100%;
+                height: 98%;
                 background: linear-gradient(0deg, ${COLORS.black}, transparent);
                 opacity: 0.8;
               }
