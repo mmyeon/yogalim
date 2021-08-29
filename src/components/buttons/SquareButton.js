4a13626151f98da92073aec5011a72213e56f2b8
mmyeon <mindfulyeon@gmail.com>
2021-08-29 18:51:51 +0900

    [fix] prop 인식되지 않는 오류 해결

--- src/components/buttons/SquareButton.js
+++ src/components/buttons/SquareButton.js
@@ -3,12 +3,11 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { COLORS, FONT_WEIGHT } from "../../styles/constant";
 
-const StyledButton = styled(Link)`
+const StyledLink = styled(Link)`
   color: ${COLORS.white};
   padding: 0.8em 0.9em;
   border-radius: 8px;
   border: none;
-  background: ${(props) => props.backgroundColor};
   font-size: 1.2em;
   cursor: pointer;
   font-weight: ${FONT_WEIGHT.bold};
@@ -17,9 +16,9 @@ const StyledButton = styled(Link)`
 
 const SquareButton = ({ to, text, backgroundColor }) => {
   return (
-    <StyledButton to={to} backgroundColor={backgroundColor}>
+    <StyledLink to={to} style={{ backgroundColor: backgroundColor }}>
       {text}
-    </StyledButton>
+    </StyledLink>
   );
 };
 
