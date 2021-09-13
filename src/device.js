dd122c3a22f2ea99652e76a989de72da61d04143
mmyeon <mindfulyeon@gmail.com>
2021-09-13 14:54:26 +0900

    [refactor] 오타 수정

--- src/device.js
+++ src/device.js
@@ -1,10 +1,10 @@
-export const DEVICE_WIDHT_MOBILE_L = 480;
+export const DEVICE_WIDTH_MOBILE_L = 480;
 export const DEVICE_WIDTH_TABLET_M = 630;
 export const DEVICE_WIDTH_TABLET_L = 768;
 export const DEVICE_WIDTH_DESKTOP = 1024;
 
 export const device = {
-  mobileL: `(min-width: ${DEVICE_WIDHT_MOBILE_L}px)`,
+  mobileL: `(min-width: ${DEVICE_WIDTH_MOBILE_L}px)`,
   tabletM: `(min-width: ${DEVICE_WIDTH_TABLET_M}px)`,
   tabletL: `(min-width: ${DEVICE_WIDTH_TABLET_L}px)`,
   desktop: `(min-width: ${DEVICE_WIDTH_DESKTOP}px)`,
